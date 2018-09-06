493ebe0777dd07191358213f4d5bf0f069796ce2
Petr Penzin <petr.penzin@intel.com>
2018-09-06 12:54:00 -0700

    Switch demo to working on a big matrix
    Vary (via a variable parameter) the matrix size. For now only do scalar
    transpose, since it is the only thing that works.
    
    Add scalar multiplication, but there are issues with it: a bug is
    blocking nested loop and the pointer parameters at this point a mushed
    together (as a potential workaround that did not work out).

--- demo.js
+++ demo.js
@@ -1,7 +1,23 @@
-function print_4(arr) {
+function init(arr, size) {
+  for (let i = 0; i < (size*size); ++i) {
+    arr[i] = i % size;
+  }
+}
+
+function verify(arr, size) {
+  for (let i = 0; i < (size*size); ++i) {
+    if (arr[i] !== i % size) {
+      print("Index: " + i + ", value: " + arr[i]);
+      return false;
+    }
+  }
+  return true;
+}
+
+function print_4(arr, off) {
   print("2x2 data: ");
-  print(arr[0] + " " + arr[1]);
-  print(arr[2] + " " + arr[3]);
+  print(arr[off]     + " " + arr[off + 1]);
+  print(arr[off + 2] + " " + arr[off + 3]);
 }
 
 function print_16(arr) {
@@ -12,75 +28,26 @@ function print_16(arr) {
   print(arr[12] + " " + arr[13] + " " + arr[14] + " " + arr[15]);
 }
 
-const N = 1000000;
-const INITIAL_SIZE = 1;
-const memObj = new WebAssembly.Memory({initial:INITIAL_SIZE});
+const N = 20000; // Matrix size
+let pages = Math.ceil(N * N / 16384.0);
+print("Need " + pages + " pages");
+const memObj = new WebAssembly.Memory({initial:pages});
 const module = new WebAssembly.Module(readbuffer('matrices.wasm'));
 const instance = new WebAssembly.Instance(module, { "dummy" : { "memory" : memObj } }).exports;
 let data = new Float32Array (memObj.buffer);
 
-for (let i = 0; i < 16; ++i) {
-  data[i] = i + 1.5;
-}
+print("Matrix size is " + N);
 
-print_4(data);
-
-print("==== 2x2 f32 WASM scalar transpose ====");
+init(data, N);
 
 var tStart = Date.now();
-for (let i = 0; i < N; ++i) {
-  instance["transpose_f32"](0, 2);
-  instance["transpose_f32"](0, 2);
-}
+instance["transpose_f32"](0, N);
+instance["transpose_f32"](0, N);
 var tEnd = Date.now();
-print((2 * N) + " transpositions took " + (tEnd - tStart) + " milliseconds.");
+print("Two transpositions took " + (tEnd - tStart) + " milliseconds.");
 
-print_4(data);
-
-/*
-print("==== 2x2 f32 WASM vector transpose ====");
-
-var tStart = Date.now();
-for (let i = 0; i < N; ++i) {
-  instance["simd_transpose_f32x2x2"]();
-  instance["simd_transpose_f32x2x2"]();
+if (verify(data, N)) {
+  print("Verification passed");
+} else {
+  print("Verification failed");
 }
-var tEnd = Date.now();
-print((2 * N) + " transpositions took " + (tEnd - tStart) + " milliseconds.");
-
-print_4(data);
-
-print("\n");
-
-for (let i = 0; i < 16; ++i) {
-  data[i] = i + 1.5;
-}
-
-*/
-print_16(data);
-
-print("==== 4x4 f32 WASM scalar transpose ====");
-
-var tStart = Date.now();
-for (let i = 0; i < N; ++i) {
-  instance["transpose_f32"](0, 4);
-  instance["transpose_f32"](0, 4);
-}
-var tEnd = Date.now();
-print((2 * N) + " transpositions took " + (tEnd - tStart) + " milliseconds.");
-
-print_16(data);
-
-/*
-print("==== 4x4 f32 WASM vector transpose ====");
-
-var tStart = Date.now();
-for (let i = 0; i < N; ++i) {
-  instance["simd_transpose_f32x4x4"](0, 0);
-  instance["simd_transpose_f32x4x4"](0, 0);
-}
-var tEnd = Date.now();
-print((2 * N) + " transpositions took " + (tEnd - tStart) + " milliseconds.");
-
-print_16(data);
-*/
